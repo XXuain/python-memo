ed1e5696cbc451aabf6d19d37fa5d8320be8966f
yellowHuang <hlady1115@gmail.com>
2020-11-08 13:14:46 +0800

    open file

--- open_file.py
+++ open_file.py
@@ -1,6 +1,10 @@
 data = []
+count = 0
 with open('test.txt', 'r') as file:
   for f in file:
     data.append(f.strip())
+    count += 1
+    if count % 1000 == 0:
+      print(len(data))
 
 print(data, len(data))
