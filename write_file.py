5bdd5a2e4eb2452e3847ce7e593d4e598df230d6
yellow <yellow.huang@hwaotech.com>
2020-11-09 17:55:41 +0800

    write file add encoding

--- write_file.py
+++ write_file.py
@@ -10,6 +10,7 @@ print(products)
 for p in products:
     print(p[0], 'price is: ', p[1])
 
-with open('products.csv', 'w') as file:
+with open('products.csv', 'w', encoding='utf-8') as file:
+    file.write('商品,價格\n')
     for f in products:
         file.write(f[0] + ',' + p[1] + '\n')  # csv 用逗點做區隔
