6aaba9ebf646a299c30177c6025bec4cefbe4747
yellowHuang <hlady1115@gmail.com>
2020-11-13 16:44:18 +0800

    sms handler

---
+++ smsHandle/smsTry.py
@@ -0,0 +1,15 @@
+from twilio.rest import Client
+
+# Your Account SID from twilio.com/console
+account_sid = "account_sid"
+# Your Auth Token from twilio.com/console
+auth_token = "auth_token"
+
+client = Client(account_sid, auth_token)
+
+message = client.messages.create(
+    to="+886123456789",
+    from_="+1234567890",
+    body="Hello from Python!")
+
+print(message.sid)
