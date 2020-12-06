8806d01152310687c9d0ee1fe57258a868d27fca
yellowHuang <hlady1115@gmail.com>
2020-12-06 18:01:54 +0800

    dunder methods: add, mul

--- advanced/dunderMethods.py
+++ advanced/dunderMethods.py
@@ -10,7 +10,24 @@ class Product:
     def __repr__(self):  # 顯示詳細版 可用 print(repr(CLASS))
         return f'<Product({self.name}, ${self.price}) >'
 
+    def __add__(self, other):
+        if isinstance(other, str):  # 如果 other 是 字串的實例
+            self.name += other
+        if isinstance(other, Product):
+            return [self, other]
 
-p = Product('水果茶', 80)
-print(p)
-print(repr(p))
+    def __mul__(self, other):
+        res = []
+        if isinstance(other, int):
+            for _ in range(other):
+                res.append(self)
+        return res
+
+
+p1 = Product('水果茶', 80)
+p2 = Product('義大利麵', 200)
+p1 + '檸檬'
+print(p1 + p2)
+print(p1 * 5)
+print(p1)
+# print(repr(p))
